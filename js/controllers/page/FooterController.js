2bb379c9910aa10761b52f9b871fc599050c2e5b
Mikhail Grinshteyn <mgrinsht@gmail.com>
2012-12-16 09:46:00 -0500

    Update js/controllers/page/FooterController.js

--- js/controllers/page/FooterController.js
+++ js/controllers/page/FooterController.js
@@ -6,28 +6,30 @@ define([
   'text!views/page/FooterView.html'
 ], function ($, _, Backbone, OwnerModel, footerTemplate) {
 
-    var view = Backbone.View.extend({
-        el: $("#footer"),
-
-        initialize: function () {
-            var that = this;
-            var options = { query: MG.Config.User }
-            var onDataHandler = function (collection) {
-                that.render();
+    return function () {
+        var view = Backbone.View.extend({
+            el: $("#footer"),
+            initialize: function () {
+                var that = this;
+                var options = { query: MG.Config.User }
+                var onDataHandler = function (collection) {
+                    that.render();
+                }
+                this.model = new OwnerModel(options);
+                this.model.fetch({ success: onDataHandler, dataType: "jsonp" });
+            },
+            render: function () {
+                var data = {
+                    owner: this.model.toJSON(),
+                    _: _
+                };
+                var compiledTemplate = _.template(footerTemplate, data);
+                this.$el.html(compiledTemplate).fadeIn(2000);
             }
-            this.model = new OwnerModel(options);
-            this.model.fetch({ success: onDataHandler, dataType: "jsonp" });
-        },
-
-        render: function () {
-            var data = {
-                owner: this.model.toJSON(),
-                _: _
-            };
-            var compiledTemplate = _.template(footerTemplate, data);
-            this.$el.html(compiledTemplate).fadeIn(2000);
-        }
-    });
-    return view;
+        });
+        this.init = function () {
+            var v = new view()
+        };
+    };
 
 });
