07037e5ddae599e8905f21ada766e1b28d7b805f
Mikhail Grinshteyn <mgrinsht@gmail.com>
2012-12-15 23:14:55 -0500

    Update js/controllers/page/NavController.js

--- js/controllers/page/NavController.js
+++ js/controllers/page/NavController.js
@@ -5,20 +5,24 @@
   'text!views/page/NavView.html'
 ], function ($, _, Backbone, NavView) {
 
-    var view = Backbone.View.extend({
-        el: $("#nav"),
-        render: function () {
-            this.$el.html(NavView);
+    return function(){
+        var view = Backbone.View.extend({
+            el: $("#nav"),
+            render: function () {
+                this.$el.html(NavView);
+            }
+        });
+        this.init = function(){
+            var v = new view()
+            v.render()
             $('.menu li').click(function () {
-                var _this = this;
                 $('.views').hide();
                 $('.' + $(this).attr('view')).show();
                 $('.menu li').removeClass('active');
                 $(this).addClass('active');
             });
-        }
-    });
-
-    return view;
+        };
+    };
 
 });
+
